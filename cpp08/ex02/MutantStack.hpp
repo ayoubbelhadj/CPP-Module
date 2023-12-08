4193abe614f70d7637465755bacb8f2c4ec80ea4
abelhadj <abelhadj@student.1337.ma>
2023-12-08 23:51:28 +0100

    Stack

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,33 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+#include <iostream>
+
+template <class T>
+class MutantStack : public std::stack<T>
+{
+public:
+	MutantStack(){}
+	MutantStack(const MutantStack<T>& obj){
+		*this = obj;
+	}
+	MutantStack<T>& operator=(const MutantStack<T>& obj){
+		this->c = obj.c;
+		return *this;
+	}
+	~MutantStack(){}
+
+	typedef typename std::stack<T>::container_type::iterator	iterator;
+
+	iterator begin(){
+		return this->c.begin();
+	}
+
+	iterator end(){
+		return this->c.end();
+	}
+};
+
+
+#endif
